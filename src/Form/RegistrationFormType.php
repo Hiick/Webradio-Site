e15832405da8902e79de89635be543600c63c580
Maysoon <maysoondoucanse@gmail.com>
2020-05-13 10:30:32 +0200

    resolution du probleme dans le controller musiclibrary

---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('avatar')
+            ->add('username')
+            ->add('email')
+            ->add('channels')
+            ->add('role')
+            ->add('password')
+            ->add('status')
+            ->add('slug')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
